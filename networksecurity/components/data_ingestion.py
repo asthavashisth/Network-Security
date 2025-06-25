ac651511db908ba0e06db54a72dbd92684676414
asthavashisth <astha.vashisth136@gmail.com>
2025-06-25 10:42:55 +0530

    Data Transformation done

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -29,7 +29,7 @@ class DataIngestion:
             collection_name=self.data_ingestion_config.collection_name
             self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
             collection= self.mongo_client[database_name][collection_name]
-            df = pd.DataFrame(list(collection.find()))
+            df = pd.DataFrame(list(collection.find().limit(5)))
             if "_id" in df.columns.to_list():
                 df=df.drop(columns=["_id"], axis=1)
             df.replace({"na":np.nan},inplace=True)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -25,3 +25,24 @@ def write_yaml_file(file_path: str, content:object,replace:bool=False) -> None:
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
+    
+def save_numpy_array(file_path:str,array:np.array):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path , "wb") as file_obj:
+            np.save(file_obj,array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+def save_object(file_path:str,obj:object)->None:
+    try:
+        logger.info("Entered save object method")
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path , "wb") as file_obj:
+            pickle.dump(obj,file_obj)
+        logger.info("Exited save object method")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
